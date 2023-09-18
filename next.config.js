5faefed69fd38d857e207f212f586edeeb548101
iamefe <efeativie@gmail.com>
2023-09-18 12:45:12 +0100

    Deleted toml and edit next config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "/solve-crowdfunding-dapp" : "",
 };
 
 module.exports = nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = nextConfig
