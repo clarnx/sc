47a86d78fb558cbfb079a8c54dde3ffa41e759e8
iamefe <efeativie@gmail.com>
2023-09-18 12:20:18 +0100

    Edit Next Config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/solve-crowdfunding-dapp" : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
